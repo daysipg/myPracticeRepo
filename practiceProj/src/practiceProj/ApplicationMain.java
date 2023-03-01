e7b69ee40df4161aa0136bc8b0c3f9bf214ca2cc
Daysi Perez <98241116+daysipg@users.noreply.github.com>
2023-02-28 20:13:57 -0800

    initial commit

---
+++ practiceProj/src/practiceProj/ApplicationMain.java
@@ -0,0 +1,19 @@
+package practiceProj;
+
+public class ApplicationMain {
+	public static void main(String[] args) {
+		String[] shopList = new String[] {
+				"Apples",
+				"Bananas",
+				"Corn",
+				"Dog Food",
+				"Cereal", 
+				"Bread", 
+				"Eggs",
+				"Milk"
+		};
+		for (String item: shopList) {
+			System.out.println(item);
+		}	
+       }
+}
